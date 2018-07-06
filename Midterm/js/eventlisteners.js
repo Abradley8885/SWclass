42e6c7943b5617913344c05bfc3b49e7e7fae2dd
Abradley8885 <abradley8885@conestogac.on.ca>
2018-07-06 15:32:55 -0400

    6

--- Midterm/js/eventlisteners.js
+++ Midterm/js/eventlisteners.js
@@ -6,13 +6,18 @@ var elUsername = document.getElementById('username'); // form text input
 var elPassword = document.getElementById('password'); // form text input
 var elUsernameMsg = document.getElementById('userfeedback'); // Text feedback on user name input
 var elPasswordMsg = document.getElementById('passwordfeedback')// Text feedback on password input
+var elSubmitMsg = document.getElementById('submitfeedback')// Text feedback on password input
 
+var enableU = 0;
+var enableP = 0;
 // Functions to call
 function checkUsername(minlength) {   // Function with parameter, without event object passed
   if(elUsername.value.length < minlength && elUsername.value.length > 0) {
     elUsernameMsg.innerHTML = 'min ' + minlength.toString() + ' characters';
+    enableU = 0;
   } else if (elUsername.value.length > minlength){
     elUsernameMsg.innerHTML = 'Username: OK'; // OK message
+    enableU = 1;
   } else {
     elUsernameMsg.innerHTML = ''; // Clear any previous error message
 
@@ -25,33 +30,44 @@ function checkPassword(e, minlength) {
   var character = '';
   var upperCase = false;
   var number = false;
-
   if(elPassword.value.length < minlength && elPassword.value.length > 0) {
-    elPasswordMsg.innerHTML = 'min ' + minlength.toString() + ' characters';;
+    elPasswordMsg.innerHTML = 'min ' + minlength.toString() + ' characters';
   } else if (elPassword.value.length > minlength) {
-    // If Password is at least minlength characters long,
-    // check to make sure it has at least one upper case character
-    // and one number
     for(i = 0; i < elPassword.value.length; i++) {
       character = elPassword.value.charAt(i);
-      if (character == character.toUpperCase()) {
+      if (character == character.toUpperCase() && isNaN(character * 1)) {
         upperCase = true;
       }
       if (!isNaN(character * 1)) {
         number = true;
       }
     }
-    if (upperCase == false || number == false) {
-      elPasswordMsg.innerHTML = 'Password must have at least 1 upper case character and 1 number';
-    } else {
+    if (upperCase == true && number == true) {
       elPasswordMsg.innerHTML = 'Password: OK';
+      enableP = 1;
+    } else {
+      elPasswordMsg.innerHTML = 'Password must have at least 1 upper case character and 1 number';
+      enableP = 0;
+
+      //document.getElementById("register-submit").disabled = false;
+
+
     }
 
   } else {  // Password field is clear
     elPasswordMsg.innerHTML = ''; // Clear any previous error message
   }
 }
+function checkForm(e){
+  if(enableU==1 && enableP == 1){
+      document.getElementById("register-submit").disabled = false;
+  }
+  else{
+      document.getElementById("register-submit").disabled = true;
+
+  }
+}
 
 // Add event listeners to the elements
-elUsername.addEventListener('blur', function() {checkUsername(7)}, false);  // Blur event occurs when user click off this element
-elPassword.addEventListener('blur', function(e){checkPassword(e, 7)}, false);
+elUsername.addEventListener('blur', function() {checkUsername(7); checkForm(e)}, false);  // Blur event occurs when user click off this element
+elPassword.addEventListener('blur', function(e){checkPassword(e, 7); checkForm(e)}, false);
--- Midterm/php/members.php
+++ Midterm/php/members.php
@@ -1,3 +1,35 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name='description' content='Login page for SW Midterm' />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Member Page</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="../css/log_req_styles.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <!--<script src='../js/log_req_func.js'></script>-->
+</head>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Midterm</a>
+      <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#collapse-navbar" aria-expanded="false" aria-controls="navbar">
+        &#9776;
+      </button>
+    </div>
+    <div class="navbar-collapse collapse" id="collapse-navbar">
+    <ul class="nav navbar-nav">
+      <li><a href="/SWclass/Midterm/php/members.php">Members</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="/SWclass/Midterm/html/signup.html"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="/SWclass/Midterm/html/login.html"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+  </div>
+</nav>
+</html>
 <?php
 session_start();
 if(isset($_SESSION['username'])){
@@ -6,6 +38,7 @@ if(isset($_SESSION['username'])){
   exit();
 }
 else{
+  echo "<h1>You must sign in to see the contents of this page</h1>";
   exit();
 }
 ?>
