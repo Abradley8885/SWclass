3b057004b99479c3d2d6c3a24fea2129f0897a5e
Abradley8885 <abradley8885@conestogac.on.ca>
2018-08-01 12:24:34 -0400

    Assignment 2

--- Quiz2/php/members.php
+++ Quiz2/php/members.php
@@ -10,25 +10,25 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <!--<script src='../js/log_req_func.js'></script>-->
 
-<title>Sign Up</title>
+<title>SWQuiz2</title>
 
 </head>
 
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">Midterm</a>
+      <a class="navbar-brand" href="#">Quiz2</a>
       <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#collapse-navbar" aria-expanded="false" aria-controls="navbar">
         &#9776;
       </button>
     </div>
     <div class="navbar-collapse collapse" id="collapse-navbar">
     <ul class="nav navbar-nav">
-      <li><a href="/SWclass/Midterm/php/members.php">Members</a></li>
+      <li><a href="/SWclass/Quiz2/php/members.php">Members</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="/SWclass/Midterm/html/signup.html"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="/SWclass/Midterm/html/login.html"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li><a href="/SWclass/Quiz2/html/signup.html"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="/SWclass/Quiz2/html/login.html"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
     </ul>
   </div>
   </div>
@@ -42,7 +42,7 @@
 					<div class="panel-heading">
 						<div class="row">
 							<div class="col-xs-16">
-								<a href="#" class="active" id="register-form-link">Sign Up</a>
+								<a href="#" class="active" id="register-form-link">Welcome to the Members only page!</a>
 							</div>
 						</div>
 						<hr>
@@ -51,30 +51,40 @@
 						<div class="row">
 							<div class="col-lg-12">
 
-								<form id="register-form" action="../php/addcredentials.php" method="post" role="form" style="display: block;">
+								<form id="register-form" action="members.php" method="post" role="form" style="display: block;">
+                  <div class="container">
+                    <h2>Control the elevator</h2>
+                      <div class="radio">
+                        <label><input type="radio" name="optradio" value="1">Floor 1</label>
+                      </div>
+                      <div class="radio">
+                        <label><input type="radio" name="optradio" value="2">Floor 2</label>
+                      </div>
+                      <div class="radio">
+                        <label><input type="radio" name="optradio" value="3">Floor 3</label>
+                      </div>
 
-                  <span id='userfeedback'></span><br/>
-									<div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-										<input type="text" name="Rusername" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
-									</div>
+                  </div>
 
-                  <span id='passwordfeedback'></span><br/>
-									<div class="input-group">
-                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-										<input type="password" name="Rpassword" id="password" tabindex="2" class="form-control" placeholder="Password">
-									</div>
 
                   <br/>
 									<div class="form-group">
 										<div class="row">
 											<div class="col-sm-6 col-sm-offset-3">
-												<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Add Me" disabled>
+												<input type="submit" name="update" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Update car Node">
 											</div>
 										</div>
 									</div>
-								</form>
+							</form>
 							</div>
+              <?php
+              if(isset($_POST['update'])){
+                update($_POST['optradio']);
+                echo "The car is now on floor:" . $_POST['optradio'];
+                displayTable();
+              }
+
+               ?>
 						</div>
 					</div>
           <footer>
@@ -82,10 +92,8 @@
               Copyright &copy ABradley
             </p>
           </footer>
-          <span id='submitfeedback'></span><br/>
 				</div>
-        <p align="center"><i>Username and password must be at least 7 characters<br/>Password must have at least one UPPER case letter and 1 number</i></p>
-			</div>
+
 		</div>
 	</div>
   <script src='../js/eventlisteners.js'></script>
@@ -93,17 +101,48 @@
 </html>
 
 <?php
-session_start();
-if(isset($_SESSION['username'])){
-  echo "<h1>Welcome " . $_SESSION['username'] . " to the members only page</h1>";
-  echo "<a href='logout.php' class='btn btn-danger' role='button'>Log Out</a>";
-  exit();
-}
-else{
-  echo "<h1>You must sign in to see the contents of this page</h1>";
-  exit();
-}
 
 
+function update(int $newFloor){
+
+  $db = new PDO('mysql:host=127.0.0.1;dbname=quizElevator', 'root', '');//open database
+
+  $query = 'UPDATE carNode SET floorNumber = :new WHERE nodeID = 1';//set query to update current floor
+
+  $stmt = $db->prepare($query);
+  $stmt->bindvalue('new', $newFloor);
+
+  $stmt->execute();
+
+}
+
+function displayTable(){
+  $db = new PDO('mysql:host=127.0.0.1;dbname=quizElevator', 'root', '');//open database
+  echo '
+  <table class="table responsive">
+  	<thead>
+    	<tr>
+				<th scope="col">nodeID						</th>
+      	<th scope="col">info	     	</th>
+        <th scope="col">status	     	</th>
+        <th scope="col">floorNumber	     	</th>
+    	</tr>
+  	</thead>';
+  $rows = $db->query("SELECT t1.nodeID, t1.info, t1.status, t2.floorNumber FROM elevatorNodes t1 LEFT JOIN carNode t2 ON t1.nodeID = t2.nodeID");//pull data from database
+  echo '<tbody>';
+    foreach ($rows as $row) { //while data is pulled fill table rows
+      echo//Display table
+      '<tr>
+       <td>' . $row['nodeID'] . 	'</td>
+       <td>' . $row['info'] . 	'</td>
+       <td>' . $row['status'] . 	'</td>
+       <td>' . $row['floorNumber'] . 	'</td>
+      </tr>';
+    }
+    echo '</tbody>
+      </table>';
+
+
+}
 
 ?>
